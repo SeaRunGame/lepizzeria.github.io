fd41c013a5ba78e01f5e00a96ff0de7f32e67a86
lepizzeria <162125826+lepizzeria@users.noreply.github.com>
2024-03-06 13:14:18 +0200

    Update main.js

--- site/js/main.js
+++ site/js/main.js
@@ -1,34 +1,51 @@
 function HandleEvent(event) {
-    if (event === "check_log_status") {
-        if ("logged_in" in localStorage) {
+    if (event === "check_log_status") 
+    {
+        if ("logged_in" in localStorage) 
+        {
             if (localStorage.getItem("logged_in") === "") {
                 document.getElementById("logged_in_page").style.display = 'block';
-                document.getElementById("log_in_link").style.display = 'block'; // Show login link
+                document.getElementById("log_in_link").style.visibility = 'visible'; // Show login link
             } else {
                 HandleEvent("logged_in");
             }
-        } else {
+        } else 
+        {
             document.getElementById("logged_in_page").style.display = 'block';
-            document.getElementById("log_in_link").style.display = 'block'; // Show login link
+            document.getElementById("log_in_link").style.visibility = 'visible'; // Show login link
         }
-    } else if (event === "logged_in") {
+    } 
+    else if (event === "logged_in") 
+    {
+        console.log("In logged in")
         // Show logged in elements
         document.getElementById("logged_in_page").style.display = 'block';
 
         // Hide login link
-        document.getElementById("log_in_link").style.display = 'none';
+        var loginLink = document.getElementById("log_in_link");
+        if (loginLink) 
+        {
+            loginLink.style.visibility = 'hidden';
+        }
 
         // Set the welcome text
         document.getElementById("username-text").textContent = "Welcome, " + localStorage.getItem("logged_in") + "!";
-    } else if (event === "log_out") {
+    } 
+    else if (event === "log_out") 
+    {
         // Hide logged in elements
         document.getElementById("logged_in_page").style.display = 'none';
 
         // Show login link
-        document.getElementById("log_in_link").style.display = 'block';
+        var loginLink = document.getElementById("log_in_link");
+        if (loginLink) {
+            loginLink.style.visibility = 'visible';
+        }
 
         localStorage.setItem("logged_in", "");
-    } else {
+    } 
+    else 
+    {
         document.write("Error 404 Unexpected event call");
     }
 }
