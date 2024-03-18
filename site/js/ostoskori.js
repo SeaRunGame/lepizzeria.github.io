03ac53ad93aa5bf99202c6e9b0aa2934cb528a05
lepizzeria <162125826+lepizzeria@users.noreply.github.com>
2024-03-18 11:20:39 +0200

    update

---
+++ site/js/ostoskori.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", LoadCart());
+
+function LoadCart()
+{
+    var cart_text = document.getElementById("ostoskori_text");
+    var cart_list = document.getElementById("cart_list");
+    var order_num = 0;
+
+    if ("orders" in localStorage)
+    {
+        let orders_raw = localStorage.getItem("orders").split("/");
+        let orders = {};
+        let currentKey = "";
+
+        //Debug
+        alert(JSON.stringify(orders_raw));
+
+        for (let i = 0; i < orders_raw.length; i++) 
+        {
+            let [key, value] = orders_raw[i].split(":");
+            key = key.trim();
+            value = value.trim();
+
+            if (key === "Tilaus" && value === "0") 
+            {
+                order_num++;
+                currentKey = "Tilaus " + order_num;
+                orders[currentKey] = [];
+            } 
+            else 
+            {
+                orders[currentKey].push(key + " : " + value);
+            }
+        }
+        //Debug
+        alert(JSON.stringify(orders));
+
+        for (let key in orders) 
+        {
+            if (orders.hasOwnProperty(key)) 
+            {
+                if (key.startsWith("Tilaus")) 
+                {
+                    var orderTitle = document.createElement("li");
+                    orderTitle.textContent = key;
+                    cart_list.appendChild(orderTitle);
+                }
+                let items = orders[key];
+                for (let i = 0; i < items.length; i++) 
+                {
+                    var cartItem = document.createElement("li");
+                    cartItem.textContent = items[i];
+                    cart_list.appendChild(cartItem);
+                }
+            }
+        }
+    }
+    else
+    {
+        cart_text.textContent = "Et ole tilannut mitään";
+    }
+}
